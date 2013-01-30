58fe511e6c865ac3210ac45aa26cc0ad87c07227
xbvc <vollcorn@yahoo.com>
2013-01-30 13:13:00 +0100

    update

--- setup.sh
+++ setup.sh
@@ -4,26 +4,26 @@
 
 while :
 do
-echo "ATTENTION: This script deletes the original dotfiles!"
-echo
-echo "Symlink:"
 echo 
-echo "1. .bashrc"
-echo "2. .xinit"
-echo "3. .Xresources"
-echo "4. .i3"
-echo "5. .vim & .vimrc"
-echo "6. X"
-echo "7. X"
-echo "8. Exit"
+echo -e "\e[00;31mATTENTION: This script deletes the original dotfiles!\e[00m"
+echo 
+echo "Symlink:"
+echo "1: .bashrc"
+echo "2: .xinit"
+echo "3: .Xresources"
+echo "4: .i3"
+echo "5: .vim & .vimrc"
+echo "6: X"
+echo "7: X"
+echo "8: Exit"
 read opt
 case $opt in
 1) cd && rm .bashrc && ln -s .dotfiles/bashrc .bashrc;;
 2) cd && rm .xinitrc && ln -s .dotflies/xinitrc .xinitrc;;
 3) cd && rm .Xresources && ln -s .dotflies/Xresources .Xresources;;
-4) cd && rm -rf .i3 && ln -s .dotfiles/i3/.i3 .i3
+4) cd && rm -rf .i3 && ln -s .dotfiles/i3/.i3 .i3;;
 5) cd && rm -rf .vim && rm .vimrc && ln -s .dotfiles/vim/.vim .vim && ln -s 
-.dotfiles/vim/.vimrc .vimrc
+.dotfiles/vim/.vimrc .vimrc;;
 
 8) exit;;
 *) echo "$opt is  Unknown"
